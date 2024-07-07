a90ae7e6c68184c4284970e814813aa384ebc795
Kiran Kumar <saikiran7008@gmail.com>
2024-07-07 12:51:46 +0530

    Add SA creation batch file

---
+++ Chapter-1/sa_creation_in_windows.bat
@@ -0,0 +1,10 @@
+REM This command retrieves the email of the service account named "terraform@" using the "gcloud iam service-accounts list" command
+for /f "tokens=*" %i in ('gcloud iam service-accounts list --format="value(email)" --filter="name:terraform@"') do set GOOGLE_SERVICE_ACCOUNT=%i
+
+REM This command retrieves the current Google Cloud project using the "gcloud info" command
+for /f "tokens=*" %i in ('gcloud info --format="value(config.project)"') do set GOOGLE_CLOUD_PROJECT=%i
+
+REM This command adds the service account to the IAM policy binding of the Google Cloud project with the "roles/editor" role
+gcloud projects add-iam-policy-binding %GOOGLE_CLOUD_PROJECT% --member="serviceAccount:%GOOGLE_SERVICE_ACCOUNT%" --role="roles/editor"
+
+REM $PLACEHOLDER$
